40f9db699d6ea04cea78860f6e7025b06821d0d0
Shivasharan2265 <shivasharan2265@gmail.com>
2024-03-31 02:41:48 +0530

    d

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.ATLASDB_URL);
     console.log("success");
   } catch (error) {
     console.log(`ERROR: ${error.message}`);
